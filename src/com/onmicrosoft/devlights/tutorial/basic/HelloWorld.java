233124278cbb768f263fdc6f1d81d0e388532f0b
devlights <gsf.zero1@devlights.onmicrosoft.com>
2016-09-07 11:33:45 +0900

    HelloWorldを追加.

---
+++ src/com/onmicrosoft/devlights/tutorial/basic/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.onmicrosoft.devlights.tutorial.basic;
+
+// Java 5から静的importが追加された
+import static java.lang.System.out;
+
+/**
+ * HelloWorldプログラムです。
+ * @author devlights
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String message = "Hello world";
+        out.println(message);
+    }
+}
