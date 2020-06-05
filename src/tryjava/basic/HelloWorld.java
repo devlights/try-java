98c022661f9dfb5752a6b5ffb7404d880bfe6798
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-06-05 11:16:34 +0900

    update HelloWorld.java

--- src/tryjava/basic/HelloWorld.java
+++ src/tryjava/basic/HelloWorld.java
@@ -1,16 +1,11 @@
 package tryjava.basic;
 
-// Java 5から静的importが追加された
-import static java.lang.System.out;
-
 /**
- * HelloWorldプログラムです。
- * @author devlights
+ * HelloWorld example.
  */
 class HelloWorld {
 
     public static void main(String[] args) {
-        var message = "Hello world";
-        out.println(message);
+        System.out.println("hello java");
     }
 }
