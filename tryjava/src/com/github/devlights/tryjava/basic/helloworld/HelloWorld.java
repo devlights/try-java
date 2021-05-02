53de18949b7314a58863c29d8e1356c7508f69d1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-05-02 17:56:29 +0000

    Add comment

--- tryjava/src/com/github/devlights/tryjava/basic/helloworld/HelloWorld.java
+++ tryjava/src/com/github/devlights/tryjava/basic/helloworld/HelloWorld.java
@@ -3,6 +3,9 @@ package com.github.devlights.tryjava.basic.helloworld;
 import com.github.devlights.tryjava.exceptions.ExampleFailedException;
 import com.github.devlights.tryjava.interfaces.Example;
 
+/**
+ * HelloWorldを表示します。
+ */
 public class HelloWorld implements Example {
     public void execute() throws ExampleFailedException {
         System.out.println("hello java");
