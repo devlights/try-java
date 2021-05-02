1a876d320b41c0f937f87ff978f94b388c403922
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-05-02 16:42:38 +0000

    fix

--- tryjava/src/com/github/devlights/tryjava/mapping/Builder.java
+++ tryjava/src/com/github/devlights/tryjava/mapping/Builder.java
@@ -7,7 +7,6 @@ import com.github.devlights.tryjava.interfaces.Example;
 
 public final class Builder {
     public static Map<String, Example> build() {
-        // TODO: 後々クラス名にして動的生成するべき。try-golangとかtry-csharpを参考に。
         return Map.of("helloworld", new HelloWorld());
     }
 }
