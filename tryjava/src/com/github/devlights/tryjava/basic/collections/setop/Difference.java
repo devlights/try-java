33bf2ecbb2f60aff9262d707274b1a592ca7b71f
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-05-28 11:51:20 +0900

    Add setop.Difference

--- tryjava/src/com/github/devlights/tryjava/basic/collections/setop/Difference.java
+++ tryjava/src/com/github/devlights/tryjava/basic/collections/setop/Difference.java
@@ -7,27 +7,27 @@ import com.github.devlights.tryjava.exceptions.ExampleFailedException;
 import com.github.devlights.tryjava.interfaces.Example;
 
 /**
- * Set のサンプルです. (和集合 -- Union)
+ * Set のサンプルです. (差集合 -- Difference)
  * 
  * REFERENCE:
  * https://stackoverflow.com/questions/393968/java-is-there-an-easy-quick-way-to-and-or-or-xor-together-sets
- * https://docs.oracle.com/javase/tutorial/collections/interfaces/set.html
+ * https://docs.oracle.com/javase/tutorial/collections/interfaces/set.html -
  * https://www.atmarkit.co.jp/ait/articles/1906/25/news015_3.html
  */
-public final class Union implements Example {
+public final class Difference implements Example {
 
     @Override
     public void execute() throws ExampleFailedException {
         var s1 = Set.of("golang", "java");
         var s2 = Set.of("dotnet", "python", "java");
 
-        // Java の Set で 和集合 は addAll メソッドで生成できる
-        var union = new HashSet<String>();
+        // Java の Set で 差集合 は removeAll メソッドで生成できる
+        var difference = new HashSet<String>();
 
-        union.addAll(s1);
-        union.addAll(s2);
+        difference.addAll(s1);
+        difference.removeAll(s2);
 
-        union.forEach(System.out::println);
+        difference.forEach(System.out::println);
     }
 
 }
